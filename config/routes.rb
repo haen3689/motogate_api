783563912bcd6f66673e8c7146b07eb9b32f42a7
unknown <haenlaos2024@gmail.com>
2026-08-10 11:04:15 +0700

    Add debug_admin route for diagnostic check

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: proc { [200, { "Content-Type" => "application/json" }, [{ status: "online", message: "MotoGate API Server is running" }.to_json]] }
+  get "debug_admin", to: proc { [200, { "Content-Type" => "application/json" }, [{ count: AdminUser.count, users: AdminUser.select(:id, :email, :role).as_json }.to_json]] }
 
   mount ActionCable.server => "/cable"
   devise_for :admin_users, ActiveAdmin::Devise.config
